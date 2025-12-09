0f02c798c16f25d24fb2ff6a1dee50d21e003023
Aeiddius <adriantagayom@gmail.com>
2025-12-09 15:00:25 +0800

    new

---
+++ scripts/source1.js
@@ -0,0 +1,14 @@
+
+class Source {
+    constructor() {
+        this.name = "source1";
+        this.baseUrl = "https://example.com";
+        this.lang = "en";
+        this.version = 1;
+    }
+
+    getBrowse() {}
+    getChapters() {}
+    getNovel() {}
+    getChapterContent() {}
+}
---
+++ scripts/source3.js
@@ -0,0 +1,14 @@
+
+class Source {
+    constructor() {
+        this.name = "source3";
+        this.baseUrl = "https://example.com";
+        this.lang = "tl";
+        this.version = 1;
+    }
+
+    getBrowse() {}
+    getChapters() {}
+    getNovel() {}
+    getChapterContent() {}
+}
